b9f8f8b99bd450e1386a8dea0456bf0ac87e4b94
Projit <projit1234@hotmail.com>
2018-09-12 20:19:46 +0530

    integrating summarizer

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import time
 import urllib.request
+from summarizer import summarizer
 from flask import Flask, render_template, redirect, jsonify, request
 from flask_cors import CORS
 import subprocess
@@ -28,14 +30,17 @@ def parse():
 def imgProc():
     url = request.form["pageData"]
     urllib.request.urlretrieve(url, "./temp")
-
-    subprocess.call(["tesseract", "./temp", "out"])
+    try:
+        subprocess.call(["tesseract", "./temp", "out"])
+        time.sleep(4)
+    except:
+        print("Give better file please")
     f = open('out.txt', 'r')
     return parsify(f.read())
 
 
 def parsify(text):
-    return "hello world" + text
+    return summarizer(text) 
 
 
 
