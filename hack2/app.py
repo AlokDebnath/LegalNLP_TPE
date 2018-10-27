24807d6e594b7709b4d7710ea0b3ec92ca29a48c
Projit <projit1234@hotmail.com>
2018-10-27 23:34:34 +0530

    3 views, working

--- hack2/app.py
+++ hack2/app.py
@@ -262,6 +262,10 @@ def process(file_path, summary_length):
 ### App handing of the user interactions
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/index/')
 def index():
     return render_template('index.html')
 
